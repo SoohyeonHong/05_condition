2fb4f61d4770b170ff373447205f94b410821abb
SoohyeonHong <55083734+SoohyeonHong@users.noreply.github.com>
2019-10-01 15:39:08 +0900

    02

--- main.c
+++ main.c
@@ -3,18 +3,47 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+
 int main(int argc, char *argv[]) {
+	
+	
+	//  #1
 	
 	int input; 
 	
-	printf("input a number : ");
-	scanf("%d", &input);
+		printf("input a number : ");
+		scanf("%d", &input);
 	
 	
-	if(input>0)
-	printf("positive.\n");
+	if(input > 0)
+	
+		printf("positive.\n");
+	
 	else
-	printf("non-positive.\n");
+	
+		printf("non-positive.\n");
+	
+
+
+  	//  #2
+	
+	int input2;
+	
+		printf("input a number : ");
+		scanf("%d" , &input2);
+		
+	if(input2 > 0)
+	
+		printf("%d\n", input2);
+		
+	else if(input2 < 0)
+	
+		printf("%d\n", -input2);
+	
+	
+	
+	
+	
 	
 	return 0;
 }
